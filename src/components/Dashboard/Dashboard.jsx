831810213e8027445d7f4312279fab9bfaac0b89
Leo <leonid.samograew@gmail.com>
2021-10-19 15:04:36 +0300

    added request to jsonplaceholder with display response in a new component with props

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,12 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Dashboard.css'
+import ResponseDB from './ResponseDB/ResponseDB';
 
 export default function Dashboard() {
+  const [state, setstate] = useState(0);
+  const [response, setresponse] = useState('empty');
+
+  function addRequest(input) {
+    fetch(`https://jsonplaceholder.typicode.com/todos/${input}`)
+      .then(setresponse('waiting for response'))
+      .then(response => response.json())
+      .then(json => json.title ? setresponse(json.title) : setresponse("nothing to write"))
+  }
   return (
     <div className="Dashboard">
       <div className="Dashboard-content">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi, accusamus id sint perferendis explicabo cumque voluptatibus voluptates. Dolore aut nesciunt animi, sequi neque illum autem sit error recusandae mollitia commodi?
+        <div>
+          <h1>{state}</h1>
+          <button
+            onClick={() => setstate(state + 1)}
+          >
+            increment
+          </button>
+          <button
+            onClick={() => setstate(state - 1)}
+            style={{ marginLeft: '5px' }}
+          >
+            decrement
+          </button>
+          <br />
+        </div>
+        <div className="jsonplaceholder">
+          <h1>API JSONPlaceHolder</h1>
+          <input className="jsonplaceholder__input"
+            type="text"
+            onChange={
+              (e) => {
+                addRequest(e.target.value)
+              }
+            }
+          />
+          <br />
+          <ResponseDB some={response} />
+        </div>
       </div>
-    </div>
+
+    </div >
   )
 }
---
+++ src/components/Dashboard/ResponseDB/ResponseDB.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export default function ResponseDB(props) {
+  return (
+    <h1 style={{ display: 'inline' }}>Respone:{props.some}</h1>
+  );
+}
