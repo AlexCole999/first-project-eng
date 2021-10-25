f5fc889a1e33eb6e6f1abede6e685d726c20d083
Leo <leonid.samograew@gmail.com>
2021-10-25 17:14:07 +0300

    chenged css in some files

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,12 @@ import './Footer.css'
 export default function Footer() {
   return (
     <div className="Footer">
-      <div className="Footer-word">FOOTER</div>
-    </div>
+      <div className="Footer-word">
+        <div style={{ float: 'right' }}> Test Single Page Application</div><br />
+        <div style={{ float: 'right' }}> developed by Leonid S.</div><br />
+        <div style={{ float: 'right' }}>+7-950-038-31-73 </div><br />
+        <div style={{ float: 'right' }}>Saint Petersburg</div><br />
+      </div>
+    </div >
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,15 +3,16 @@ import './Navbar.css'
 import NavbarElem from './NavbarElem/NavbarElem'
 import Logo from './Logo/Logo'
 
+
 export default function Navbar() {
   return (
     <div className="Navbar">
       <Logo />
       <div className="Navbar-container">
-        <NavbarElem name="1th" />
-        <NavbarElem name="2th" />
-        <NavbarElem name="3th" />
-        <NavbarElem name="4th" />
+        <NavbarElem name="Добавить слова" />
+        <NavbarElem name="Список добавленных" />
+        <NavbarElem name="Игра" />
+        <NavbarElem name="Настройки аккаунта" />
       </div>
     </div>
   )
