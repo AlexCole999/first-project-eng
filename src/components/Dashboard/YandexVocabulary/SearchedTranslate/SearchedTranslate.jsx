e89530cbaa9780d242f8caf53b2e1fd7a9e93eda
Leo <leonid.samograew@gmail.com>
2021-11-03 13:54:34 +0300

    pre-final translate component

--- src/components/Dashboard/YandexVocabulary/SearchedTranslate/SearchedTranslate.jsx
+++ src/components/Dashboard/YandexVocabulary/SearchedTranslate/SearchedTranslate.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import './SearchedTranslate.css';
+import toUpperCase from '../../../functionsForComponents/toUpperCase';
+
 // import { AiFillCheckCircle } from "react-icons/ai";
 
 export default function SearchedTranslate(props) {
   return (
     <div className="SearchedTranslate">
       <div>
-        {props.translates.map(x => <div>{x.text}</div>)}
+        <div className="SearchedTranslate__pos">
+          {props.pos == "noun"
+            ? "Существительное"
+            : props.pos == "adjective"
+              ? "Прилагательное"
+              : props.pos == "verb"
+                ? "Глагол"
+                : props.pos == "pronoun"
+                  ? "Местоимение"
+                  : props.pos == "adverb"
+                    ? "Наречие" : props.pos}
+        </div>
+        {props.translates.map(
+          x =>
+            <div>
+              {toUpperCase(x.text)}
+            </div>)}
       </div>
-
     </div>
   );
 }
--- src/components/Dashboard/YandexVocabulary/YandexVocabulary.jsx
+++ src/components/Dashboard/YandexVocabulary/YandexVocabulary.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import SearchedTranslate from './SearchedTranslate/SearchedTranslate';
 import './YandexVocabulary.css';
-import toUpperCase from './../../functionsForComponents/toUpperCase';
 
 export default function YandexVocabulary(props) {
   return (
     <div className="YandexVocabulary">
-      Возможный перевод:
-      <br />
       {props.data.code !== 502  /* <<<---Обработка пробелов */
-        ? (props.data.def.length > 0 ? props.data.def.map(x => <SearchedTranslate translates={x.tr} />) : 'Других переводов не найдено')
+        ? (props.data.def.length > 0 ? props.data.def.map(x => <SearchedTranslate pos={x.pos} translates={x.tr} />) : 'Других переводов не найдено')
         : 'Других переводов не найдено'}
     </div>
   );
