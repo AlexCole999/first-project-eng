325eb04cc02d9ffa8b936931ceac45d3e29033b8
Leo <leonid.samograew@gmail.com>
2021-11-04 21:15:41 +0300

    ---

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Navbar.css'
 import Logo from './Logo/Logo'
 import { AiOutlineFileSearch } from "react-icons/ai"
-import { AiOutlineSetting } from "react-icons/ai"
+import { AiOutlineUser } from "react-icons/ai"
 import { AiOutlineHdd } from "react-icons/ai"
 import { AiOutlineFund } from "react-icons/ai"
 
@@ -16,7 +16,7 @@ export default function Navbar() {
           <a href="/" className="Navbar-right-elem" title="Искать слова"><AiOutlineFileSearch style={{ width: "55px", height: "55px" }} /></a>
           <a href="/" className="Navbar-right-elem" title="Добавленные слова"><AiOutlineHdd style={{ width: "55px", height: "55px" }} /></a>
           <a href="/" className="Navbar-right-elem" title="Учить (Мой прогресс)"><AiOutlineFund style={{ width: "55px", height: "55px" }} /></a>
-          <a href="/" className="Navbar-right-elem" title="Настройки (Профиль)"><AiOutlineSetting style={{ width: "55px", height: "55px" }} /></a>
+          <a href="/" className="Navbar-right-elem" title="Настройки (Профиль)"><AiOutlineUser style={{ width: "55px", height: "55px" }} /></a>
         </div>
       </div>
     </div>
