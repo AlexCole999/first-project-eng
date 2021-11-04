baee88e16c31ac07550406af890b5b749b755e27
Leo <leonid.samograew@gmail.com>
2021-11-04 18:50:09 +0300

    transfered logic from dashboard into YandexVocabulary component for Route preparing

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,49 +1,16 @@
 import React, { useState } from 'react'
 import './Dashboard.css'
 import YandexVocabulary from './YandexVocabulary/YandexVocabulary';
-import toUpperCase from '../functionsForComponents/toUpperCase';
+
 
 export default function Dashboard() {
-  const [yandex, setyandex] = useState({ head: {}, def: [] });
 
-  function yandexDictionaryRequest(input) {
-    if (input) {
-      fetch
-        ('https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=\dict.1.1.20210811T164421Z.dc92c34aa55f8bde.11d283af044e951db1e180d89d183eafd3dac943&lang=en-ru&text=' + input)
-        .then(x => x.json())
-        .then(x => {
-          console.log("------------");
-          console.log(x);
-          setyandex(x);
-        }
-        );
-
-    }
-    else setyandex({ head: {}, def: [] });
-  }
   return (
     <div className="dashboard-container">
       <div className="Dashboard">
         <div className="Dashboard-content">
-          <div className="wordSearch">
-            <div>Введите слово для поиска:</div>
-            <input className="wordSearch__input"
-              type="text"
-              onChange={
-                (e) => {
-                  yandexDictionaryRequest(e.target.value);
-                }
-              }
-            />
-            <div className="mainTranslate">
-              {yandex.code !== 502
-                ? (yandex.def.length ? `${toUpperCase(yandex.def[0].tr[0].text)}` : "Нет в словаре")
-                : "Нет в словаре"}
-            </div>
-
-          </div>
-          <YandexVocabulary data={yandex} />
-          <button onClick={() => console.log(yandex)}></button>
+          <YandexVocabulary />
+          <button onClick={() => console.log(1)}></button>
         </div>
 
       </div >
--- src/components/Dashboard/YandexVocabulary/YandexVocabulary.jsx
+++ src/components/Dashboard/YandexVocabulary/YandexVocabulary.jsx
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchedTranslate from './SearchedTranslate/SearchedTranslate';
 import './YandexVocabulary.css';
+import toUpperCase from '../../functionsForComponents/toUpperCase';
 
 export default function YandexVocabulary(props) {
+  const [yandex, setyandex] = useState({ head: {}, def: [] });
+
+  function yandexDictionaryRequest(input) {
+    if (input) {
+      fetch
+        ('https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=\dict.1.1.20210811T164421Z.dc92c34aa55f8bde.11d283af044e951db1e180d89d183eafd3dac943&lang=en-ru&text=' + input)
+        .then(x => x.json())
+        .then(x => {
+          console.log("------------");
+          console.log(x);
+          setyandex(x);
+        }
+        );
+
+    }
+    else setyandex({ head: {}, def: [] });
+  }
   return (
-    <div className="YandexVocabulary">
-      {props.data.code !== 502  /* <<<---Обработка пробелов */
-        ? (props.data.def.length > 0 ? props.data.def.map(x => <SearchedTranslate pos={x.pos} translates={x.tr} key={x.pos} />) : 'Других переводов не найдено')
-        : 'Других переводов не найдено'}
+    <div>
+      <div className="wordSearch">
+        <div>Введите слово для поиска:</div>
+        <input className="wordSearch__input"
+          type="text"
+          onChange={
+            (e) => {
+              yandexDictionaryRequest(e.target.value);
+            }
+          }
+        />
+        <div className="mainTranslate">
+          {yandex.code !== 502
+            ? (yandex.def.length ? `${toUpperCase(yandex.def[0].tr[0].text)}` : "Нет в словаре")
+            : "Нет в словаре"}
+        </div>
+
+      </div>
+      <div className="YandexVocabulary">
+        {yandex.code !== 502  /* <<<---Обработка пробелов */
+          ? (yandex.def.length > 0 ? yandex.def.map(x => <SearchedTranslate pos={x.pos} translates={x.tr} key={x.pos} />) : 'Других переводов не найдено')
+          : 'Других переводов не найдено'}
+      </div>
     </div>
   );
 }
