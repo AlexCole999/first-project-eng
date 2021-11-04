c590c5a61f081ec1763232c503109233830379bd
Leo <leonid.samograew@gmail.com>
2021-11-04 21:45:12 +0300

    Router --- deleted and transfered Navbar component into App component directly

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,27 @@
-import Navbar from '../Navbar/Navbar';
 import Dashboard from '../Dashboard/Dashboard';
 import Footer from '../Footer/Footer';
 import './App.css';
+import React from 'react';
+import Logo from './Logo/Logo';
+import { AiOutlineFileSearch } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { AiOutlineHdd } from "react-icons/ai";
+import { AiOutlineFund } from "react-icons/ai";
 
 function App(props) {
   return (
     <div className="App">
-      <Navbar />
+      <div className="navbar-container">
+        <div className="Navbar">
+          <Logo />
+          <div className="Navbar-right">
+            <a href="/" className="Navbar-right-elem" title="Искать слова"><AiOutlineFileSearch style={{ width: "55px", height: "55px" }} /></a>
+            <a href="/" className="Navbar-right-elem" title="Добавленные слова"><AiOutlineHdd style={{ width: "55px", height: "55px" }} /></a>
+            <a href="/" className="Navbar-right-elem" title="Учить (Мой прогресс)"><AiOutlineFund style={{ width: "55px", height: "55px" }} /></a>
+            <a href="/" className="Navbar-right-elem" title="Настройки (Профиль)"><AiOutlineUser style={{ width: "55px", height: "55px" }} /></a>
+          </div>
+        </div>
+      </div>
       <Dashboard />
       <Footer />
     </div>
