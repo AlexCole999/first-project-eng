5aa864bed10f199a15343adba3115636a4ec3e83
Leo <leonid.samograew@gmail.com>
2021-11-04 19:27:15 +0300

    installed react-router-dom

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -3,11 +3,11 @@ import './Dashboard.css'
 import YandexVocabulary from './YandexVocabulary/YandexVocabulary';
 
 export default function Dashboard() {
-
   return (
     <div className="dashboard-container">
       <div className="Dashboard">
         <div className="Dashboard-content">
+
           <YandexVocabulary />
           <button onClick={() => console.log(1)}></button>
         </div>
