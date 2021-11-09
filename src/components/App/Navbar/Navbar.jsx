69f782f18b3272220a05ed5c162f1d32afd59c60
Leo <leonid.samograew@gmail.com>
2021-11-09 14:49:12 +0300

    added Navbar selection highlighting

--- src/components/App/Navbar/Navbar.jsx
+++ src/components/App/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { AiOutlineFileSearch } from "react-icons/ai";
 import { AiOutlineUser } from "react-icons/ai";
 import { AiOutlineHdd } from "react-icons/ai";
 import { AiOutlineFund } from "react-icons/ai";
-import { NavLink } from "react-router-dom";
+import { NavLink, Routes, Route } from "react-router-dom";
 
 export default function Navbar() {
   return (
@@ -15,12 +15,54 @@ export default function Navbar() {
             <a href="/">ENGLISH</a>
           </div>
         </div>
-        <div className="Navbar-right">
-          <NavLink to={"/addword"} className="Navbar-right-elem" title="Искать слова"><AiOutlineFileSearch style={{ width: "55px", height: "55px" }} /></NavLink>
-          <NavLink to={"/mywords"} className="Navbar-right-elem" title="Добавленные слова"><AiOutlineHdd style={{ width: "55px", height: "55px" }} /></NavLink>
-          <NavLink to={"/progress"} className="Navbar-right-elem" title="Учить (Мой прогресс)"><AiOutlineFund style={{ width: "55px", height: "55px" }} /></NavLink>
-          <NavLink to={"/profile"} className="Navbar-right-elem" title="Настройки (Профиль)"><AiOutlineUser style={{ width: "55px", height: "55px" }} /></NavLink>
-        </div>
+        <Routes>
+          <Route
+            exact path="/"
+            element={<div className="Navbar-right">
+              <NavLink to={"/addword"} className="Navbar-right-elem" title="Искать слова"><AiOutlineFileSearch style={{ width: "55px", height: "55px" }} /></NavLink>
+              <NavLink to={"/mywords"} className="Navbar-right-elem" title="Добавленные слова"><AiOutlineHdd style={{ width: "55px", height: "55px" }} /></NavLink>
+              <NavLink to={"/progress"} className="Navbar-right-elem" title="Учить (Мой прогресс)"><AiOutlineFund style={{ width: "55px", height: "55px" }} /></NavLink>
+              <NavLink to={"/profile"} className="Navbar-right-elem" title="Настройки (Профиль)"><AiOutlineUser style={{ width: "55px", height: "55px" }} /></NavLink>
+            </div>}>
+          </Route>
+          <Route
+            exact path="/addword"
+            element={<div className="Navbar-right">
+              <NavLink to={"/addword"} style={{ border: "3px solid rgb(111, 111, 220)", backgroundColor: "white", color: "rgb(34, 85, 224)" }} className="Navbar-right-elem" title="Искать слова"><AiOutlineFileSearch style={{ width: "55px", height: "55px" }} /></NavLink>
+              <NavLink to={"/mywords"} className="Navbar-right-elem" title="Добавленные слова"><AiOutlineHdd style={{ width: "55px", height: "55px" }} /></NavLink>
+              <NavLink to={"/progress"} className="Navbar-right-elem" title="Учить (Мой прогресс)"><AiOutlineFund style={{ width: "55px", height: "55px" }} /></NavLink>
+              <NavLink to={"/profile"} className="Navbar-right-elem" title="Настройки (Профиль)"><AiOutlineUser style={{ width: "55px", height: "55px" }} /></NavLink>
+            </div>}>
+          </Route>
+          <Route
+            exact path="/mywords"
+            element={<div className="Navbar-right">
+              <NavLink to={"/addword"} className="Navbar-right-elem" title="Искать слова"><AiOutlineFileSearch style={{ width: "55px", height: "55px" }} /></NavLink>
+              <NavLink to={"/mywords"} style={{ border: "3px solid rgb(111, 111, 220)", backgroundColor: "white", color: "rgb(34, 85, 224)" }} className="Navbar-right-elem" title="Добавленные слова"><AiOutlineHdd style={{ width: "55px", height: "55px" }} /></NavLink>
+              <NavLink to={"/progress"} className="Navbar-right-elem" title="Учить (Мой прогресс)"><AiOutlineFund style={{ width: "55px", height: "55px" }} /></NavLink>
+              <NavLink to={"/profile"} className="Navbar-right-elem" title="Настройки (Профиль)"><AiOutlineUser style={{ width: "55px", height: "55px" }} /></NavLink>
+            </div>}>
+          </Route>
+          <Route
+            exact path="/progress"
+            element={<div className="Navbar-right">
+              <NavLink to={"/addword"} className="Navbar-right-elem" title="Искать слова"><AiOutlineFileSearch style={{ width: "55px", height: "55px" }} /></NavLink>
+              <NavLink to={"/mywords"} className="Navbar-right-elem" title="Добавленные слова"><AiOutlineHdd style={{ width: "55px", height: "55px" }} /></NavLink>
+              <NavLink to={"/progress"} style={{ border: "3px solid rgb(111, 111, 220)", backgroundColor: "white", color: "rgb(34, 85, 224)" }} className="Navbar-right-elem" title="Учить (Мой прогресс)"><AiOutlineFund style={{ width: "55px", height: "55px" }} /></NavLink>
+              <NavLink to={"/profile"} className="Navbar-right-elem" title="Настройки (Профиль)"><AiOutlineUser style={{ width: "55px", height: "55px" }} /></NavLink>
+            </div>}>
+          </Route>
+          <Route
+            exact path="/profile"
+            element={<div className="Navbar-right">
+              <NavLink to={"/addword"} className="Navbar-right-elem" title="Искать слова"><AiOutlineFileSearch style={{ width: "55px", height: "55px" }} /></NavLink>
+              <NavLink to={"/mywords"} className="Navbar-right-elem" title="Добавленные слова"><AiOutlineHdd style={{ width: "55px", height: "55px" }} /></NavLink>
+              <NavLink to={"/progress"} className="Navbar-right-elem" title="Учить (Мой прогресс)"><AiOutlineFund style={{ width: "55px", height: "55px" }} /></NavLink>
+              <NavLink to={"/profile"} style={{ border: "3px solid rgb(111, 111, 220)", backgroundColor: "white", color: "rgb(34, 85, 224)" }} className="Navbar-right-elem" title="Настройки (Профиль)"><AiOutlineUser style={{ width: "55px", height: "55px" }} /></NavLink>
+            </div>}>
+          </Route>
+        </Routes>
+
       </div>
     </div>
   )
