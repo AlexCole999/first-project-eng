fabf68c8da38afc16f2ac7001624e6931b8c9bee
Leo <leonid.samograew@gmail.com>
2021-12-01 16:41:12 +0300

    created MyWords component files

--- src/components/App/Dashboard/Dashboard.jsx
+++ src/components/App/Dashboard/Dashboard.jsx
@@ -3,6 +3,7 @@ import './Dashboard.css';
 import { Routes, Route } from "react-router-dom";
 import AddWord from './AddWord/AddWord';
 
+
 export default function Dashboard() {
   return (
     <div className="dashboard-container">
---
+++ src/components/App/Dashboard/MyWords/MyWords.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Dashboard.css';
+import { Routes, Route } from "react-router-dom";
+import AddWord from './AddWord/AddWord';
+
+export default function Dashboard() {
+  return (
+    <div className="MyWords">
+      123
+    </div>
+  )
+}
