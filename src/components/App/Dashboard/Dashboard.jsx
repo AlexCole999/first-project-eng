436508cae5d15a17d920cad906f9d1586f6bfc58
Leo <leonid.samograew@gmail.com>
2021-12-01 16:34:03 +0300

    deleted YandexVocabulary component, added AddWord component

--- src/components/App/Dashboard/Dashboard.jsx
+++ src/components/App/Dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Dashboard.css';
 import { Routes, Route } from "react-router-dom";
-import AddWord from './YandexVocabulary/AddWord';
+import AddWord from './AddWord/AddWord';
 
 export default function Dashboard() {
   return (
