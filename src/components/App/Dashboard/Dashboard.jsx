22be383022244249a4a0228ad9b8436760fe0ee5
Leo <leonid.samograew@gmail.com>
2021-12-05 11:43:05 +0300

    added firebase props fo all components

--- src/components/App/Dashboard/Dashboard.jsx
+++ src/components/App/Dashboard/Dashboard.jsx
@@ -16,11 +16,11 @@ export default function Dashboard(props) {
             </Route>
             <Route exact path="/first-project-eng/addword" element={<AddWord firebase={props.firebase} />}>
             </Route>
-            <Route exact path="/first-project-eng/mywords" element={<MyWords />}>
+            <Route exact path="/first-project-eng/mywords" element={<MyWords firebase={props.firebase} />}>
             </Route>
-            <Route exact path="/first-project-eng/progress" element={<Progress />}>
+            <Route exact path="/first-project-eng/progress" element={<Progress firebase={props.firebase} />}>
             </Route>
-            <Route exact path="/first-project-eng/profile" element={<Profile />}>
+            <Route exact path="/first-project-eng/profile" element={<Profile firebase={props.firebase} />}>
             </Route>
           </Routes>
         </div>
--- src/components/App/Dashboard/MyWords/MyWords.jsx
+++ src/components/App/Dashboard/MyWords/MyWords.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './MyWords.css';
 
-export default function MyWords() {
+export default function MyWords(props) {
+  function getDataFromFirebase(){}
   return (
     <div className="MyWords">
       MyWords
+      <button onClick={()=>console.log(1)}></button>
     </div>
   )
 }
