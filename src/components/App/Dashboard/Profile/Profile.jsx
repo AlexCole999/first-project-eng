1d86ce2f2e4e2429778d059540197db716a3e2f4
Leo <leonid.samograew@gmail.com>
2021-12-13 18:23:12 +0300

    created Profile components functions: display length of appended words with useEffect

--- src/components/App/Dashboard/Profile/Profile.jsx
+++ src/components/App/Dashboard/Profile/Profile.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import './Profile.css';
+import { collection, getDocs } from 'firebase/firestore';
 
-export default function Profile() {
+export default function Profile(props) {
+  const initialState = 'Загрузка данных'
+  const [state, setstate] = useState(initialState)
+  useEffect(getData)
+  function getData() {
+    getDocs(collection(props.firebase, "users", "user", "appendedwords"))
+      .then(x => {
+        setstate(x.docs.length); console.log(x)
+      })
+  }
   return (
     <div className="Profile">
-      Profile
+      <button onClick={getData}>getData</button>
+      <div>{state}</div>
     </div>
   )
 }
--- src/components/App/Dashboard/Progress/Progress.jsx
+++ src/components/App/Dashboard/Progress/Progress.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Progress.css';
 
+
+
 export default function Progress() {
   return (
     <div className="Progress">
