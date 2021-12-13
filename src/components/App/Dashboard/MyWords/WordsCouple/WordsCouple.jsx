2c65031ef493bdf09404741b4e92a07abd182326
Leo <leonid.samograew@gmail.com>
2021-12-13 18:09:52 +0300

    ---

--- src/components/App/Dashboard/MyWords/WordsCouple/WordsCouple.jsx
+++ src/components/App/Dashboard/MyWords/WordsCouple/WordsCouple.jsx
@@ -6,8 +6,8 @@ import toUpperCase from './../../../../functionsForComponents/toUpperCase';
 export default function WordsCouple(props) {
   return (
     <div className="WordsCouple">
-      <div>{toUpperCase(props.word)}</div>
-      <div style={{ fontStyle: "italic", fontWeight: "600" }}>
+      <div style={{ display: 'flex', alignItems: 'center' }}>{toUpperCase(props.word)}</div>
+      <div style={{ fontStyle: "italic", fontWeight: "500" }}>
         {Array.isArray(props.translate)
           ? props.translate.map(x =>
             <div key={x} style={{ display: 'flex', justifyContent: 'flex-end' }}>
