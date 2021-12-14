d270f1f6e3a9b8e8bd9cd4c2a196941e7f3d0162
Leo <leonid.samograew@gmail.com>
2021-12-14 21:58:36 +0300

    Profile component --->>> created statistic view

--- src/components/App/Dashboard/Profile/Profile.jsx
+++ src/components/App/Dashboard/Profile/Profile.jsx
@@ -5,19 +5,25 @@ import { collection, getDocs } from 'firebase/firestore';
 export default function Profile(props) {
 
   const initialState = 'загрузка...'
-  const [state, setstate] = useState(initialState)
+  const [vocabularyLength, setVocabularyLength] = useState(initialState)
   useEffect(getData, [])
 
   function getData() {
     getDocs(collection(props.firebase, "users", "user", "appendedwords"))
       .then(x => {
-        setstate(x.docs.length); console.log(x)
+        setVocabularyLength(x.docs.length); console.log(x)
       })
   }
   return (
     <div className="Profile">
-      <div style={{ display: 'flex', justifyContent: 'center' }}><div>Статистика</div></div>
-      <div>Слов в словаре: {state}</div>
+      <div className="statistic">
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <div>Статистика</div>
+        </div>
+        <div className='statistic-item'>
+          <div>Слов в словаре: </div><div><b>{vocabularyLength}</b></div>
+        </div>
+      </div>
     </div>
   )
 }
