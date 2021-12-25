9854561b5f6ca96ce9108180ade8e3990f68c02f
Leo <leonid.samograew@gmail.com>
2021-12-25 21:33:31 +0300

    fixed footer

--- src/components/App/Footer/Footer.jsx
+++ src/components/App/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Footer.css'
-
+import { AiFillPhone } from 'react-icons/ai'
 export default function Footer() {
   return (
     <div className="footer-container">
@@ -8,7 +8,7 @@ export default function Footer() {
         <div className="Footer-word">
           <div style={{ float: 'right' }}> Test Single Page Application</div><br />
           <div style={{ float: 'right' }}> developed by Leonid S.</div><br />
-          <div style={{ float: 'right' }}>+7-950-038-31-73 </div><br />
+          <div style={{ float: 'right', display: 'flex', alignItems: 'center' }}><AiFillPhone style={{ color: 'rgb(101, 253, 40, 0.9)' }} />+7-950-038-31-73</div><br />
           <div style={{ float: 'right' }}>Saint Petersburg</div><br />
         </div>
       </div>
