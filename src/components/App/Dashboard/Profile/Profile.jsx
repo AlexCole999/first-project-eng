638277ac460d066b68f5b0ffb49cc8675fcfbf57
Leo <leonid.samograew@gmail.com>
2022-01-19 20:06:48 +0300

    REDUX for Profile.jsx DONE

--- src/components/App/Dashboard/Profile/Profile.jsx
+++ src/components/App/Dashboard/Profile/Profile.jsx
@@ -1,4 +1,4 @@
-import { React, useState, useEffect } from 'react';
+import { React, useEffect } from 'react';
 import './Profile.css';
 import { collection, getDocs } from 'firebase/firestore';
 import { useDispatch, useSelector } from 'react-redux';
