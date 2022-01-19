c4987f045c8d636b4ea75c3c4569a21e7b683909
Leo <leonid.samograew@gmail.com>
2022-01-19 21:29:09 +0300

    some fixes for reducers initialstate

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App.jsx';
-import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from "react-router-dom";
 import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { BrowserRouter } from "react-router-dom";
 import combinedReducers from './reducers/combinedReducers';
 
 const store = createStore(combinedReducers, composeWithDevTools(applyMiddleware(thunk)))
--- src/reducers/getWordsFromFirebaseReducer.js
+++ src/reducers/getWordsFromFirebaseReducer.js
@@ -1,5 +1,5 @@
-let defaultstateone = { data: [] }
-const getWordsFromFirebaseReducer = (state = defaultstateone, action) => {
+let defaultstate = { data: [] }
+const getWordsFromFirebaseReducer = (state = defaultstate, action) => {
   switch (action.type) {
     case "ADD_DATA_FROM_FIREBASE":
       return { ...state, data: action.data }
