f26fbbce4c91a47d4502320065d6f8b3a196249a
Leo <leonid.samograew@gmail.com>
2022-01-21 18:14:12 +0300

    WordsCouple.jsx --->>> prepared for delete functionality

--- src/components/App/Dashboard/MyWords/WordsCouple/WordsCouple.jsx
+++ src/components/App/Dashboard/MyWords/WordsCouple/WordsCouple.jsx
@@ -2,8 +2,18 @@ import { React } from 'react';
 import './WordsCouple.css';
 import toUpperCase from './../../../../functionsForComponents/toUpperCase';
 import { AiFillCloseCircle } from "react-icons/ai";
+import { getDoc, doc } from 'firebase/firestore';
+import { useSelector } from 'react-redux';
 
 export default function WordsCouple(props) {
+
+  const firebase = useSelector(state => state.firebase.firebase)
+
+  async function some(x) {
+    let a = await getDoc(doc(firebase, "users", "user", "appendedwords", props.word));
+    console.log(a.data().translate)
+    console.log(x)
+  }
   return (
     <div className="WordsCouple">
       <div className='WordsCouple-word'>{toUpperCase(props.word)}</div>
@@ -11,7 +21,7 @@ export default function WordsCouple(props) {
         {Array.isArray(props.translate)
           ? props.translate.map(x =>
             <div key={x} className='WordsCouple-translates-translate'>
-              <div>{toUpperCase(x)}</div><AiFillCloseCircle className='WordsCouple-icon' style={{ marginLeft: '10px' }} />
+              <div>{toUpperCase(x)}</div><AiFillCloseCircle onClick={() => some(x)} className='WordsCouple-icon' style={{ marginLeft: '10px' }} />
             </div>
           )
           : toUpperCase(props.translate)}
