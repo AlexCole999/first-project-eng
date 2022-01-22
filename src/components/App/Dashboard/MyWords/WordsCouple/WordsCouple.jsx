c99d8d20cd8b0dde3a39294ddda4fe06f373345c
Leo <leonid.samograew@gmail.com>
2022-01-22 15:10:08 +0300

    MyWords -- WordsCouple --->>> DONE deletetion functionality

--- src/components/App/Dashboard/MyWords/WordsCouple/WordsCouple.jsx
+++ src/components/App/Dashboard/MyWords/WordsCouple/WordsCouple.jsx
@@ -2,30 +2,47 @@ import { React } from 'react';
 import './WordsCouple.css';
 import toUpperCase from './../../../../functionsForComponents/toUpperCase';
 import { AiFillCloseCircle } from "react-icons/ai";
-import { getDoc, doc } from 'firebase/firestore';
-import { useSelector } from 'react-redux';
+import { getDocs, getDoc, doc, setDoc, collection, deleteDoc } from 'firebase/firestore';
+import { useSelector, useDispatch } from 'react-redux';
 
 export default function WordsCouple(props) {
 
+  const dispatch = useDispatch()
   const firebase = useSelector(state => state.firebase.firebase)
+  const dataFromFirebase = useSelector(state => state.wordsFromFirebase.data)
 
-  async function some(x) {
-    let a = await getDoc(doc(firebase, "users", "user", "appendedwords", props.word));
-    console.log(a.data().translate)
-    console.log(x)
+  async function some(translatedWord) {
+    let translatedWordsFromFirebase = dataFromFirebase.filter(x => x.data().word == props.word)[0].data().translate
+    if (translatedWordsFromFirebase.length !== 1) {
+      await setDoc(doc(firebase, "users", "user", "appendedwords", props.word), {
+        word: props.word,
+        translate: translatedWordsFromFirebase.filter(word => word !== translatedWord)
+      })
+      getDocs(collection(firebase, "users", "user", "appendedwords"))
+        .then(collection => {
+          dispatch({ type: "ADD_DATA_FROM_FIREBASE", data: collection.docs });
+        })
+    }
+    else {
+      await deleteDoc(doc(firebase, "users", "user", "appendedwords", props.word));
+      getDocs(collection(firebase, "users", "user", "appendedwords"))
+        .then(collection => {
+          dispatch({ type: "ADD_DATA_FROM_FIREBASE", data: collection.docs });
+        })
+    }
   }
   return (
     <div className="WordsCouple">
       <div className='WordsCouple-word'>{toUpperCase(props.word)}</div>
       <div className='WordsCouple-translates'>
         {Array.isArray(props.translate)
-          ? props.translate.map(x =>
-            <div key={x} className='WordsCouple-translates-translate'>
-              <div>{toUpperCase(x)}</div><AiFillCloseCircle onClick={() => some(x)} className='WordsCouple-icon' style={{ marginLeft: '10px' }} />
+          ? props.translate.map(translatedWord =>
+            <div key={translatedWord} className='WordsCouple-translates-translate'>
+              <div>{toUpperCase(translatedWord)}</div><AiFillCloseCircle onMouseDown={() => some(translatedWord)} className='WordsCouple-icon' style={{ marginLeft: '10px' }} />
             </div>
           )
           : toUpperCase(props.translate)}
       </div>
-    </div>
+    </div >
   )
 }
